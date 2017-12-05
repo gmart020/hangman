f58ceb2ca08e59d408709951ecb2be720f6f9d98
Jerry <gmart020@ucr.edu>
2017-12-05 22:36:29 +0000

    Only login remains to be fixed

--- client.py
+++ client.py
@@ -5,13 +5,14 @@ import socket
 import threading
 
 def main_menu():
-    print ''
-    print 'Main Menu'
-    print '\t1. Login'
-    print '\t2. Make New User'
-    print '\t3. Hall of Fame'
-    print '\t4. Exit'
-    print ''
+    print '\n******************************'
+    print '*          Main Menu         *'
+    print '******************************'
+    print '1. Login'
+    print '2. Make New User'
+    print '3. Hall of Fame'
+    print '4. Exit'
+    print '******************************'
     
 HOST = '127.0.0.1'
 PORT = 8888
@@ -24,40 +25,97 @@ except socket.error, msg:
     
 s.connect((HOST, PORT))
 
-def send(message)
+def send_message(message):
     global s
     try:
         s.send(message)
     except socket.error:
         print 'Send failed'
         sys.exit()
-    reply = s.recv(4096)
+
+def receive_message():
+    global s
+    reply = s.recv(1024)
     return reply
+    
+def new_user():
+    prompt1 = 'What is your user name?: '
+    prompt2 = 'What is your password?: '
+    response = ''
+    while response != 'user created':
+        print '******************************'
+        print   '*      Create New User       *'
+        print   '******************************'
+        if response == 'username exists':
+            print 'ERROR: Username already exits. Please try again...'
+        username = raw_input(prompt1)
+        password = getpass.getpass(prompt2)
+        message = 'make' + '_' + username + '_' + password
+        send_message(message)
+        response = receive_message()
+        
+    print  '******************************'
+    print   '* User Created Successfully  *'
+    print   '******************************'
+    
+def hall_of_fame():
+    request = 'halloffame_'
+    send_message(request)
+    response = receive_message()
+    print response
+    
+def exit_game():
+    request = 'exit_'
+    send_message(request)
+    response = receive_message()
+    print response
+    s.close()
+    sys.exit()
+    
+def login():
+    request = 'login'
+    send_message(request)
+    response = receive_message()
+    response = response.split('_')
+    while response[0] != 'username valid':
+        print response[0]
+        if len(response) > 2:
+            print response[1]
+            username = raw_input(response[2])
+        else:
+            username = raw_input(response[1])
+        send_message(username)
+        response = receive_message()
+        response = response.split('_')
+        
+    response = receive_message()
+    response = response.split('_')
+    while response[0] != 'password valid':
+        print response[0]
+        if len(response) > 2:
+            print response[1]
+            password = raw_input(response[2])
+        else:
+            password = raw_input(response[1])
+        send_message(password)
+        response = receive_message()
+        response = response.split('_')
 
     
-def new_user_menu():
-    print ''
-    username = raw_input('What is your user name?\n')
-    msg = 'm_' + username
-    response = send(msg)
-    print response
-    password = getpass.getpass('What is your password?\n')
-    print ''
-    return (username, password)
+    print 'complete'
     
 while 1:
     main_menu()
-    selection = int(raw_input('\tEnter an option: '))
+    selection = int(raw_input('Enter an option: '))
     print ''
     if selection == 1:
-        pass
+        login()
     elif selection == 2:
-        username, password = new_user_menu()
-        request = 'm_' + username 
+        new_user()
     elif selection == 3:
-        pass
+        hall_of_fame()
     elif selection == 4:
-        pass
+        exit_game()
     else:
         pass
 
--- server.py
+++ server.py
@@ -2,6 +2,21 @@ import socket
 import sys
 from  threading import Thread
   
+users = {'mango123' : 'pass1', 'apeking' : 'pass2', 'penisgrabber' : 'pass3'}
+words = ['apple', 'cinammon', 'banana']
+hall_of_fame = [('apeking', 10), ('mango123', 6), ('penisgrabber', 4)]  
+
+def send_message(c, message):
+    try:
+        c.conn.send(message)
+    except socket.error:
+        print 'Send failed'
+        sys.exit()
+
+def receive_message(c):
+    reply = c.conn.recv(1024)
+    return reply
+
 def make_socket(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -11,6 +26,52 @@ def make_socket(host, port):
     s.listen(10)
     return s
 
+def hall_fame(c):
+    reply = '\n******************************\n'
+    reply +=  '*          Hall of Fame      *\n'
+    reply +=  '******************************\n'
+    for i, member in enumerate(hall_of_fame, 1):
+        reply += str(i) + '. ' + member[0] + '\tScore: ' + str(member[1]) + '\n'
+    reply +=  '******************************'
+    c.conn.send(reply)
+    
+def exit_game(c):
+    reply = '******************************\n'
+    reply +='*          Good Bye!         *\n'
+    reply +='******************************\n'    
+    send_message(c, reply)
+    
+def verify_login(c):
+    prompt1 = 'Enter your username: '
+    prompt2 = 'Enter your password: '
+    error1 = 'ERROR: User name invalid. Please try again...'
+    error2 = 'ERROR: Incorrect password for given user name. Please try again...'
+    heading = '******************************\n'
+    heading +='*           Login            *\n'
+    heading +='******************************'
+    username = ''
+    message = heading + '_' + prompt1
+    c.conn.send(message)
+    while message != 'username valid':
+        username = c.conn.recv(1024)
+        if username in users:
+            message = 'username valid'
+        else:
+            message = heading + '_' + error1 + '_' + prompt1
+        c.conn.send(message)
+        
+    password = ''
+    message = heading + '_' + prompt2
+    c.conn.send(message)
+    while message != 'password valid':
+        password = c.conn.recv(1024)
+        if users[username] != password:
+            message = heading + '_' + error2 + '_' + prompt2
+        else:
+            message = 'password valid'
+        c.conn.send(message)
+        
+    
 class ClientThread(Thread):
     
     def __init__(self, conn, addr):
@@ -18,12 +79,31 @@ class ClientThread(Thread):
         self.conn = conn
         self.addr = addr
 
+    def make_new_user(self, username, password):
+        global users
+        while username in users:
+            reply = 'username exists'
+            self.conn.send(reply)
+            data = self.conn.recv(1024)
+            username = data.split('_', 1)[1]
+            
+        users[username] = password
+        self.conn.send('user created')
+
     def run(self): 
         while True:
             data = self.conn.recv(1024)
-            reply = 'OK...' + data
-            self.conn.send(reply)
-        conn.close()
+            request = data.split('_')
+            if request[0] == 'login':
+                verify_login(self)
+            elif request[0] == 'make':
+                self.make_new_user(request[1], request[2])
+            elif request[0] == 'halloffame':
+                hall_fame(self)
+            elif request[0] == 'exit':
+                exit_game(self)
+                break
+        self.conn.close()
         
 class ConnectionListener(Thread):
     
@@ -41,28 +121,30 @@ class ConnectionListener(Thread):
 thread = ConnectionListener()
 thread.start()
 
-users = {'mango123' : 'pass1', 'apeking' : 'pass2'}
-words = ['apple', 'cinammon', 'banana']
-
 def main_menu():
-    print 'Main Menu'
-    print '\t1. Current List of Users'
-    print '\t2. Current List of Words'
-    print '\t3. Add new word to the list of words.'
+    print '\n******************************'
+    print '*          Main Menu         *'
+    print '******************************'
+    print '1. Current list of users'
+    print '2. Current list of words'
+    print '3. Add a word'
+    print '******************************'
     
 def print_users():
-    print '\nUsers'
-    print '--------------------------'
+    print '\n******************************'
+    print '*           Users            *'
+    print '******************************'
     for i, user in enumerate(users, 1):
         print str(i) + '. ' + str(user)
-    print ''
+    print '******************************'
     
 def print_words():
-    print '\nWords'
-    print '--------------------------'
+    print '\n******************************'
+    print '*           Words            *'
+    print '******************************'
     for i, word in enumerate(words, 1):
         print str(i) + '. ' + str(word)
-    print ''
+    print '******************************'
     
 def add_word():
     print ''
@@ -72,7 +154,7 @@ def add_word():
 
 while 1:
     main_menu()
-    selection = int(raw_input('\tChoose an option: '))
+    selection = int(raw_input('Choose an option: '))
     if selection == 1:
         print_users()
     elif selection == 2:
