8ed585fd5992bc6870588e984d55fffff10a7a6c
Jerry <gmart020@ucr.edu>
2017-12-08 05:39:48 +0000

    Game works for single player

--- client.py
+++ client.py
@@ -17,6 +17,8 @@ def main_menu():
 HOST = '127.0.0.1'
 PORT = 8888
 
+name = ''
+
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error, msg:
@@ -35,7 +37,7 @@ def send_message(message):
 
 def receive_message():
     global s
-    reply = s.recv(1024)
+    reply = s.recv(4096)
     return reply
     
 def new_user():
@@ -54,56 +56,155 @@ def new_user():
         send_message(message)
         response = receive_message()
         
-    print  '******************************'
+    print  '\n******************************'
     print   '* User Created Successfully  *'
     print   '******************************'
     
 def hall_of_fame():
-    request = 'halloffame_'
+    request = 'halloffame'
     send_message(request)
     response = receive_message()
     print response
     
 def exit_game():
-    request = 'exit_'
+    request = 'exit'
     send_message(request)
     response = receive_message()
     print response
     s.close()
     sys.exit()
     
+def join_game():
+    name = receive_message()
+    message = 'NEW:'+ name
+    send_message(message)
+    response = receive_message()
+    response = response.split(':')
+    while response[0] != 'game over':
+        print ''
+        print response[0]
+        input = raw_input('Enter your guess: ')
+        reply = input + ':' + response[1] + ':' 
+        send_message(reply)
+        response = receive_message()
+        response = response.split(':')
+        
+    print response[1]
+    
+def start_game():
+    response = receive_message()
+    response = response.split(':')
+    global name
+    while response[0] != 'game over':
+        print ''
+        print response[0]
+        input = raw_input('Enter your guess: ')
+        reply = input + ':' + name 
+        send_message(reply)
+        response = receive_message()
+        response = response.split(':')
+        
+    print response[1]
+        
+def select_difficulty():
+    request = 'select difficulty'
+    send_message(request)
+    resp = receive_message()
+    print resp
+    selection = raw_input('Choose an option: ')
+    if int(selection) > 3:
+        selection = '1'
+    send_message(selection)
+    start_game()
+
+def get_games():
+    request = 'getgames'
+    send_message(request)
+    response = receive_message()
+    response = response.split('>')
+    print response[0]
+    if len(response) > 1:
+        return
+    else:
+        selection = raw_input('Choose a game to join: ')
+        send_message(selection)
+        join_game()
+
+def back_to_main():
+    request = 'log out'
+    send_message(request)
+    global name
+    name = ''
+  
+def game_menu():
+    send_message('login menu')
+    menu = receive_message()
+    while 1:
+        print ''
+        print menu
+        selection = int(raw_input('Choose an option: '))
+        
+        if selection == 1:
+            select_difficulty()
+        elif selection == 2:
+            get_games()
+        elif selection == 3:
+            #finished
+            hall_of_fame()
+        elif selection == 4:
+            #finished
+            back_to_main()
+            return
+        else:
+            pass
+        
+        selection = 0
+    
 def login():
     request = 'login'
     send_message(request)
     response = receive_message()
     response = response.split('_')
-    while response[0] != 'username valid':
-        print response[0]
-        if len(response) > 2:
-            print response[1]
-            username = raw_input(response[2])
-        else:
-            username = raw_input(response[1])
+    heading = response[0]
+    prompt1 = response[1]
+    error1 = response[2]
+    prompt2 = response[3]
+    error2 = response[4]
+    confirmation = response[5]
+    
+    print heading
+    username = raw_input(prompt1)
+    send_message(username)
+    response = receive_message()
+    
+    while response != 'username valid':
+        print ''
+        print heading
+        print error1
+        username = raw_input(prompt1)
         send_message(username)
         response = receive_message()
-        response = response.split('_')
         
+    print ''
+    print heading
+    password = getpass.getpass(prompt2)
+    send_message(password)
     response = receive_message()
-    response = response.split('_')
-    while response[0] != 'password valid':
-        print response[0]
-        if len(response) > 2:
-            print response[1]
-            password = raw_input(response[2])
-        else:
-            password = raw_input(response[1])
+    
+    while response != 'password valid':
+        print ''
+        print heading
+        print error2
+        password = getpass.getpass(prompt2)
         send_message(password)
         response = receive_message()
-        response = response.split('_')
-
-    
-    print 'complete'
-    
+        
+    print ''
+    print confirmation
+    global name
+    name = username
+    game_menu()
+        
 while 1:
     main_menu()
     selection = int(raw_input('Enter an option: '))
--- server.py
+++ server.py
@@ -1,11 +1,226 @@
 import socket
 import sys
 from  threading import Thread
-  
+import random
+import uuid
+import select
+from threading import Lock
+
+game_lock = Lock()
 users = {'mango123' : 'pass1', 'apeking' : 'pass2', 'penisgrabber' : 'pass3'}
 words = ['apple', 'cinammon', 'banana']
-hall_of_fame = [('apeking', 10), ('mango123', 6), ('penisgrabber', 4)]  
+hall_of_fame = [('apeking', 10), ('mango123', 6), ('penisgrabber', 4)] 
+active_games = []
+you_lose = '\n******************************\n   You Guessed Wrong\n******************************\n'
 
+class Game(Thread):
+    
+    def __init__(self, difficulty, player):
+        self.lck = Lock()
+        tmp = player.get_name()
+        self.player_list = {}
+        self.score_list = {}
+        self.order_list = []
+        # EASY = 1, MEDIUM = 2, HARD = 3
+        self.difficulty = difficulty
+        self.word = words[random.randint(0, len(words) - 1)]
+        self.cur_word = self.encrypt()
+        self.turn = 0
+        self.max_guess = self.select_max()
+        self.guessed = set()
+        self.add_player(player, tmp)
+    
+    def select_max(self):
+        if self.difficulty == 1:
+            return len(self.word) * 3
+        elif self.difficulty == 2:
+            return len(self.word) * 2
+        else:
+            return len(self.word)
+    
+    def get_socks(self):
+        socks = []
+        self.lck.acquire()
+        for player in self.player_list:
+            socks.append(self.player_list[player].conn)
+        self.lck.release()
+        return socks
+    
+    def encrypt(self):
+        result = ''
+        for ch in self.word:
+            result += '_'
+        return result
+        
+    def add_player(self, player, name):
+        self.lck.acquire()
+        self.player_list[name] = player
+        self.order_list.append(name)
+        self.score_list[name] = 0
+        self.lck.release()
+        
+    def remove_player(self, name):
+        self.lck.acquire()
+        del self.player_list[name]
+        self.order_list.remove(name)
+        del self.score_list[name]
+        self.lck.release()
+        
+    def game_state(self):
+        screen = '******************************\n'
+        screen +='          Hangman             \n'
+        screen +='******************************\n'
+        self.lck.acquire()
+        screen = screen + ' '.join(list(self.cur_word))
+        screen += '\n\n'
+        for i, ch in enumerate(list(self.guessed), 1):
+            screen += ch
+            screen += ' '
+            if i % len(self.word) == 0:
+                screen += '\n'
+
+        screen += '\n\n'
+        
+        for i, player in enumerate(self.player_list):
+            screen = screen + player + ' ' + str(self.score_list[player])
+            if i == self.turn:
+                screen += ' *'
+            screen += '\n'
+        self.lck.release()
+        return screen
+        
+    def get_info(self):
+        self.lck.acquire()
+        w = ' '.join(list(self.cur_word))
+        d = ''
+        if self.difficulty == 1:
+            d = 'Easy'
+        elif self.difficulty == 2:
+            d = 'Medium'
+        else:
+            d = 'Hard'
+        players = ', '.join(self.order_list)
+        self.lck.release()
+        result = w + '\n' + 'Difficulty: ' + d + '\n' + 'Players: ' + players + '\n' 
+        return result
+        
+    def handle_guess(self, guess, player):
+        if player == self.order_list[self.turn]:
+            if len(guess) > 1:
+                if guess != self.word:
+                    return -1
+                else:
+                    self.cur_word = self.word
+                    self.score_list[player] += len(self.word)
+                    return 2
+            elif len(guess) == 0:
+                return 0
+            else:
+                if guess in self.guessed or guess in self.cur_word:
+                    return 0
+                else:
+                    if guess in self.word:
+                        tmp = []
+                        for i, ch in enumerate(self.word):
+                            if ch == guess:
+                                tmp.append(guess)
+                                self.score_list[player] += 1
+                            elif self.cur_word[i] != '_':
+                                tmp.append(ch)
+                            else:
+                                tmp.append('_')
+                        self.lck.acquire()
+                        self.cur_word = ''.join(tmp)
+                        self.lck.release()
+                        if '_' not in self.cur_word:
+                            return 2
+                        else:
+                            return 1
+                    else:
+                        self.guessed.add(guess)
+                        if len(self.guessed) == self.max_guess:
+                            return -2
+      
+    def send_state(self):
+        state = self.game_state()
+        for player in self.player_list:
+            message = state
+            self.player_list[player].conn.send(message)
+                            
+    def start(self):
+        self.send_state()
+        while 1:
+            socks = self.get_socks()
+            reads, writes, errors = select.select(socks, [], [])
+            while not reads:
+                pass
+                # #idk what this does
+                # socks = self.get_socks()
+                # reads, writes, errors = select.select(socks, [], [])
+            #recieve response from a client
+            response = reads[0].recv(1024)
+            response = response.split(':')
+
+            if response[0] != 'NEW':
+                guess = response[0]
+                sender = response[1]
+                result = self.handle_guess(guess, sender)
+                
+                if result == -1:
+                    self.player_list[sender].conn.send('game over:' + you_lose)
+                    self.remove_player(sender)
+                    if self.turn >= len(self.order_list) and len(self.order_list) > 0:
+                        self.turn = 0
+                    else:
+                        active_games.remove(self)
+                        # All players lost
+                        return 
+                elif result == -2:
+                    message = 'game over:'
+                    message += '\n******************************\n'
+                    message +='  Max Guess Attempts Reached\n'
+                    message +='          Game Over\n'
+                    message +='******************************\n\n'
+                    message += self.game_state()
+                    for i, player in enumerate(self.player_list):
+                        self.player_list[player].conn.send(message)
+                    game_lock.acquire()
+                    active_games.remove(self)    
+                    game_lock.release()
+                    return
+                    
+                elif result == 0:
+                    if self.turn == len(self.order_list) - 1:
+                        self.turn = 0
+                    else:
+                        self.turn += 1
+                        
+                    self.send_state()
+                        
+                elif result == 2:
+                    message = 'game over:'
+                    message += '\n******************************\n      '
+                    message += sender
+                    message += ' Wins\n'
+                    message +='******************************\n\n'
+                    message += self.game_state()
+                    for i, person in enumerate(self.player_list):
+                        self.player_list[person].conn.send(message)
+                    game_lock.acquire()
+                    active_games.remove(self)    
+                    game_lock.release()
+                    return
+                else:
+                    self.send_state()
+            else:
+                self.send_state()
+                
+            response = []
+                    
+                    
+                
+            
+            
 def send_message(c, message):
     try:
         c.conn.send(message)
@@ -50,27 +265,105 @@ def verify_login(c):
     heading +='*           Login            *\n'
     heading +='******************************'
     username = ''
-    message = heading + '_' + prompt1
+    confirmation = '******************************\n'
+    confirmation +='       User logged in.\n'
+    confirmation +='******************************'
+    
+    message = heading + '_' + prompt1 + '_' + error1 + '_' + prompt2 + '_' + error2 + '_' + confirmation
     c.conn.send(message)
-    while message != 'username valid':
+    username = c.conn.recv(1024)
+    
+    while not (username in users):
+        c.conn.send('username invalid')
         username = c.conn.recv(1024)
-        if username in users:
-            message = 'username valid'
-        else:
-            message = heading + '_' + error1 + '_' + prompt1
-        c.conn.send(message)
         
-    password = ''
-    message = heading + '_' + prompt2
-    c.conn.send(message)
-    while message != 'password valid':
+    c.conn.send('username valid')
+    password = c.conn.recv(1024)
+    
+    while users[username] != password:
+        c.conn.send('password invalid')
         password = c.conn.recv(1024)
-        if users[username] != password:
-            message = heading + '_' + error2 + '_' + prompt2
-        else:
-            message = 'password valid'
-        c.conn.send(message)
+    
+    c.set_name(username)
+    c.set_score(0)
+    c.conn.send('password valid')
+    
+def get_games(c):
+    result = '\n******************************\n'
+    result +='       Active Games         \n'
+    result +='******************************\n'
+    
+    if len(active_games) > 0:
+        game_lock.acquire()
+        for i, game in enumerate(active_games, 1):
+            result = result + str(i) + '. ' + game.get_info() + '\n'
+        game_lock.release()
+    else:
+        result += 'No Games\n'
+        result += '>NADA'
+        c.conn.send(result)
+        return
+    c.conn.send(result)
+    selection = c.conn.recv(1024)
+    selection = int(selection) - 1
+    game_lock.acquire()
+    active_games[selection].add_player(c, c.get_name())
+    game_lock.release()
         
+   
+
+def game_play(client, difficulty):
+    game = Game(difficulty, client)
+    game_lock.acquire()
+    active_games.append(game)
+    game_lock.release()
+    game.start()
+
+def get_difficulty(c):
+    menu = '\n******************************\n'
+    menu +='      Choose Difficulty       \n'
+    menu +='******************************\n'
+    menu +='1. Easy\n'
+    menu +='2. Medium\n'
+    menu +='3. Hard\n'
+    menu +='******************************'
+
+    c.conn.send(menu)
+    response = c.conn.recv(1024)
+    difficulty = int(response)
+    #name = c.get_name()
+    #c.conn.send(name)
+    game_play(c, difficulty)
+    
+def game_menu(c):
+    menu = '*****************************\n'
+    menu += '           Game Menu          \n'
+    menu += '******************************\n'
+    menu += '1. Start New Game\n'
+    menu += '2. Get List of Games\n'
+    menu += '3. Hall of Fame\n'
+    menu += '4. Exit\n'
+    menu += '******************************' 
+    request = c.conn.recv(1024)
+    c.conn.send(menu)
+    while 1:
+        #wait for option selection
+        request = c.conn.recv(1024)
+        request = request.split('_')
+        if request[0] == 'select difficulty':
+            get_difficulty(c)
+        elif request[0] == 'getgames':
+            get_games(c)
+        elif request[0] == 'halloffame':
+            hall_fame(c)
+        elif request[0] == 'log out':
+            c.set_name('')
+            c.set_score(0)
+            return
+        else:
+            pass
+        
+        request = ''
     
 class ClientThread(Thread):
     
@@ -78,6 +371,23 @@ class ClientThread(Thread):
         Thread.__init__(self)
         self.conn = conn
         self.addr = addr
+        self.username = ''
+        self.score = 0
+        
+    def get_name(self):
+        return self.username
+        
+    def get_score(self):
+        return self.score
+        
+    def get_id(self):
+        return self.id
+        
+    def set_name(self, name):
+        self.username = name
+        
+    def set_score(self, score):
+        self.score = score
 
     def make_new_user(self, username, password):
         global users
@@ -96,6 +406,7 @@ class ClientThread(Thread):
             request = data.split('_')
             if request[0] == 'login':
                 verify_login(self)
+                game_menu(self)
             elif request[0] == 'make':
                 self.make_new_user(request[1], request[2])
             elif request[0] == 'halloffame':
